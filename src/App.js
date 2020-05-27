ebd13a238b3efd23f049c5fcda0056cc892b6dd9
Murphy316 <liqi316@live.com>
2020-05-27 14:19:18 -0700

    add hook

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Re-build after apply network security policy
+          Re-build after apply network security policy. Adding hook, I feel so bad..
         </p>
         <a
           className="App-link"
