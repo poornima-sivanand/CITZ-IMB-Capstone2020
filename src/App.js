abf1d2c1f10480098f88366f1ebd6ffa44e83a96
Murphy316 <liqi316@live.com>
2020-05-27 14:55:31 -0700

    final try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Re-build after apply network security policy. last try
+          Re-build after
         </p>
         <a
           className="App-link"
