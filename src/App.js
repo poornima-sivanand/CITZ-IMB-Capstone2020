20b405c9d8eed9c61abe4b20ca80518a57b96828
Murphy316 <liqi316@live.com>
2020-05-29 11:16:16 -0700

    demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Re-build after
+          Demo
         </p>
         <a
           className="App-link"
