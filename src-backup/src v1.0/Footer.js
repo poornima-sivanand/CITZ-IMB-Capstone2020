63ea6105765fe0f4141f8fbe560ca40ceed6ff5d
Murphy316 <liqi316@live.com>
2020-06-16 11:39:56 -0700

    src-backup

---
+++ src-backup/src v1.0/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+      // Contents of the footer
+      <div id="footer">
+
+      </div>
+    );
+  }
+  
+  export default Footer;
